686ed12d61e46b2dac0ad84e924e420b910e8da0
unknown <Administrator@LAB-3PC1.labs.org>
2023-03-02 20:13:06 +0500

    first commit

---
+++ New folder/cs.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".card").mouseenter(function(){
+        $(".card1").css({"transform": "scale(1.7s)", "border":"4px solid black", "transation":"(1)"});
+
+    });
+});
